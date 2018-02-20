72629906143c3ce7c829be494e817ed7ba050a45
Christopher Tran <christopher.long.tran@gmail.com>
2018-02-20 17:58:27 -0500

    set up basic link

---
+++ project4-frontend/src/components/AddItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddItem extends Component {
+  render () {
+    return (
+      <div>
+        <h2>This is the add Item page. </h2>
+      </div>
+    )
+  }
+}
+
+export default AddItem
