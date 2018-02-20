f8dc1bbe87e9b3ec86439f79f55fa6fc4e0b9d34
Christopher Tran <christopher.long.tran@gmail.com>
2018-02-20 18:16:35 -0500

    set up basic form in add item page

--- project4-frontend/src/components/AddItem.js
+++ project4-frontend/src/components/AddItem.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
 
 class AddItem extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {value: ' '}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({value: e.target.value})
+  }
+
+  handleSubmit (e) {
+    alert(this.state.value)
+    e.preventDefault()
+  }
+
   render () {
     return (
       <div>
         <h2>This is the add Item page. </h2>
+        <div className='container'>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Add Item:
+              <input type='text' value={this.state.value} onChange={this.handleChange} className='form-control' />
+            </label><br />
+            <input type='submit' value='Submit' className='btn btn-primary' />
+          </form>
+        </div>
       </div>
     )
   }
