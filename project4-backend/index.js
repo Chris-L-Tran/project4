7d3227c7b160d4eee0600dbb88cf4e8be39cacf8
Christopher Tran <christopher.long.tran@gmail.com>
2018-02-21 11:25:38 -0500

    tested backend route

--- project4-backend/index.js
+++ project4-backend/index.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.set('port', process.env.PORT || 9000)
 
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
 app.listen(app.get('port'), () => {
   console.log(`PORT: ${app.get('port')}`)
 })
