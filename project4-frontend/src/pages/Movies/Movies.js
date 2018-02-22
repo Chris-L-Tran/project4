953651cedb320ee6d16365f05f65dce89fc8c4b8
Christopher Tran <christopher.long.tran@gmail.com>
2018-02-22 11:01:03 -0500

    created method in movies page to import data

--- project4-frontend/src/pages/Movies/Movies.js
+++ project4-frontend/src/pages/Movies/Movies.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import './Movies.css'
 
+import axios from 'axios'
+
 class Movies extends Component {
+  componentDidMount = () => {
+    let path = this.props.location.pathname
+
+    this.setState({
+      path: path
+    })
+
+    state = {
+      movies: []
+    }
+
+    axios
+      .get('http://localhost:9000')
+      .then((res.err) => {
+        this.setState({
+          movies: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  
   render () {
     return (
       <div className='Movies'>
